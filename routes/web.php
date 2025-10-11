a504124c2e77d5c39a3f60017fea59933f1b09b5
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2025-10-11 20:13:02 +0700

    Fix root route to resolve 404 error and ensure home page accessibility

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Restore the root route to fix 404 error
-Route::get('/', function () {
-    return view('home');
-});
 
-// Keep the additional route for accessing the home page at /home
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
