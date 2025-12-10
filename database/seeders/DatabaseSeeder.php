d1635474bdfbc4c807af4335464dac6fc734afa4
PliTheGeek <dhafin.kamil@gmail.com>
2025-12-10 09:33:14 +0700

    Add : Automatisation For Kas Seeder When Migrated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             // Anda bisa menambahkan seeder lain di sini
+            KasCategorySeeder::class,
         ]);
     }
 }
